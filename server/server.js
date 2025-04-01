8ff57fbc8c8bb09694c90af2d222dba382e25d2c
elenkaaa2000 <elenachokalova16@abv.bg>
2025-04-01 15:51:41 +0300

    server deployment

--- server/server.js
+++ server/server.js
@@ -1759,7 +1759,7 @@
 
     const server = http__default['default'].createServer(requestHandler(plugins, services));
 
-    const port = 3030;
+    const port = process.env.PORT || 3030;
 
     server.listen(port);
 
